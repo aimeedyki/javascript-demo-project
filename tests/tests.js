dfb7d6e71d708e85c280168e1ace2d6976770edb
Amarachi <akuwudike.a@gmail.com>
2017-05-09 02:16:57 +0100

    renamed file

--- tests/tests.js
+++ tests/tests.js
@@ -2,32 +2,32 @@
 
 'use strict';
 
-const assert = require('chai').assert;
+const should = require('chai').expect;
 const myApp = require('../app/library.js');
 
 describe("Arithmetic Functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1, 2, 3) == 2);
+     expect(myApp.computeAverage(1,2,3)).should.equal(2);
   })
   it("should return 5 as average for 3, 7, 5", function() {
-    assert(myApp.computeAverage(3, 7, 5) == 5);
+       expect(myApp.computeAverage(3,7,5)).should.equal(5);
   })
   it("should return 120 as factorial for 5", function() {
-    assert(myApp.computeFactorial(5) == 120);
+      expect(myApp.computeFactorial(5)).should.equal(120);
   })
 })
 
 describe("Temperature Conversion Functionality", function() {
   it("should return 104 for Celcius value 40", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
+    expect(myApp.convertTempCtoF(40)).should.equal(104);
   })
   it("should return 140 for Celcius value 60", function() {
-    assert(myApp.convertTempCtoF(60) == 140);
+    expect(myApp.convertTempCtoF(60)).should.equal(140);
   })
   it("should return 60 for Fahrenheit value 140", function() {
-    assert(myApp.convertTempFtoC(140) == 60);
+    myApp.convertTempFtoC(140).should.equal(60);
   })
   it("should return 40 for Fahrenheit value 104", function() {
-    assert(myApp.convertTempFtoC(104) == 40);
+    myApp.convertTempFtoC(104).should.equal(40);
   })
 })
